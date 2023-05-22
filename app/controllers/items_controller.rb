1da075575aaf7edae619f24df8897ccb12f3218c
Damien Nelson <dhnisaman@gmail.com>
2023-05-22 12:43:34 -0400

    commit tree f0dd33ea621516d73ebaf4d66182e2825da0fadc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,45 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create 
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create!(items_params)
+      render json: item, status: :created
+    end
+  end
+
+
+
+  private
+
+  def render_not_found(e)
+    render json: {error: e.message }, status: :not_found
+  end
+
+  def render_invalid(e)
+    render json: {error: e.message }, status: :unprocessable_entity
+  end
+
+  def items_params
+    params.permit(:name, :description, :price, :id, :user_id)
   end
 
 end
